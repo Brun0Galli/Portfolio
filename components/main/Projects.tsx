fcbd2c99f091dfd2f072d3a2c5826a25f96fd9a4
Bruno <0256993@up.edu.mx>
2024-08-15 10:59:09 -0600

    web

---
+++ components/main/Projects.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import ProjectCard from "../sub/ProjectCard";
+
+const Projects = () => {
+  return (
+    <div
+      className="flex flex-col items-center justify-center py-20"
+      id="projects"
+    >
+      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+        My Projects
+      </h1>
+      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
+        <ProjectCard
+          src="/NextWebsite.png"
+          title="Voice assistant"
+          description="Developed a Voice Assistant in Python for browsing, managing shortcuts and being conected with a wether API to give real-time information"
+        />
+        <ProjectCard
+          src="/nlp.jpeg"
+          title="Classification Model"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+        <ProjectCard
+          src="/powerbiproyect.png"
+          title="Power bi data analysis"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+        <ProjectCard
+          src="/SpaceWebsite.png"
+          title="Neural Network"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
